8efedd97c249c08da47b70fbe0e231a3b0c83ccd
Sara Ismail <143926419+SaraIsmail2005@users.noreply.github.com>
2026-01-13 20:38:51 +0400

    Started GUI script
    Started planning and writing the draft GUI of the protocol/ experiment.

---
+++ src/python/scripts/protocol.py
@@ -0,0 +1,32 @@
+ import pygame
+ import random 
+ import time 
+
+
+ pygame.init()
+
+ screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+ width, height = screen.get_size()
+
+ whit = (255, 255, 255)
+ black = (0, 0, 0)
+
+font = pygame.font.SysFont("None", 200)  #we can make this bigger later
+
+start_time = time.time()
+total_time = 300 # 5 mins = 300 seconds
+
+#protocol steps
+
+#blank screen for 1 minute 
+
+#display on for 3.5 seconds 
+display_text("ON")
+time.sleep(3.5)
+
+#break for 3.5 seconds 
+screen.fill(WHITE)
+time.sleep (3.5)
+#random on/off for 3.5 for the 300 seconds (5 mins) 
+while time.time() -start_time < total_time:
+    word = random.choice(["ON", "OFF"])
